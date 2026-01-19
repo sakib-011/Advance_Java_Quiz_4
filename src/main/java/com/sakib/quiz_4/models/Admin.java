4745758f92e581bdc1abd3f7e57b27dfae3da34b
sakib-011 <sakibshourov01@gmail.com>
2026-01-19 18:40:08 +0600

    new model for admin

---
+++ src/main/java/com/sakib/quiz_4/models/Admin.java
@@ -0,0 +1,17 @@
+package com.sakib.quiz_4.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+
+    private String username;
+    private String password;
+
+}
